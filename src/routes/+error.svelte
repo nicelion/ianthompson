e34b9b97cb726f554354fa0fdcef9e552e05d05a
Ian Thompson <ianthompson@nicelion.com>
2022-12-12 12:41:10 -0500

    Fixed daisy and tailwind issue, finish new sveltekit migrate

---
+++ src/routes/+error.svelte
@@ -0,0 +1,40 @@
+<!--
+ +error.svelte
+ ianthompson
+ 
+ Created by Ian Thompson on December 12th 2022
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2022 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+--->
+
+<script >
+    import { page } from '$app/stores';
+
+  console.log($page);
+</script>
+
+<div class="h-full py-24 px-16 flex flex-col items-center space-x-4 space-y-9">
+  <!-- <img src="https://thumbs.gfycat.com/HatefulSeveralIbis-max-1mb.gif" alt="" class="rounded"> -->
+  <h1 class="text-3xl font-mono">This doesn't look good :(</h1>
+  <div class="mockup-code w-full">
+      <pre class=""><code>Attempting to access {$page.url.pathname}</code></pre>
+      <pre class="text-warning"><code></code></pre>
+
+      <pre class="text-warning"><code>Error!... retrying!</code></pre>
+      <pre class="text-warning"><code>Error!... retrying!</code></pre>
+      <pre class="text-warning"><code>Error!... retrying!</code></pre>
+      <pre class="text-warning"><code>Error!... retrying!</code></pre>
+      <pre class="text-warning"><code>Error!... retrying!</code></pre>
+      <pre class="text-warning"><code></code></pre>
+
+      <pre class="text-error"><code>Error: {$page.status}: {$page.error.message}</code></pre>
+  </div>
+
+  <div class="">
+      <a href="/" class="bg-theme-primary text-xl text-white font-mono px-8 py-3 rounded-md hover:bg-blue-500 transition ease-in duration-100">Go Home</a>
+  </div>
+
+</div>
