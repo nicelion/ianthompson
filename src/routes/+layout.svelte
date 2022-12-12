341576e5fb672c4560bc33a5925c3eca07406eaa
Ian Thompson <ianthompson@nicelion.com>
2022-12-12 12:04:19 -0500

    Move favion and images and update base html

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<!--
+ +layout.svelte
+ ianthompson
+ 
+ Created by Ian Thompson on December 12th 2022
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2022 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+--->
+
+<script>
+    import "../app.css";
+</script>
+  
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -12,16 +12,19 @@
 
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from "svelte-preprocess";
 
-/** @type {import('@sveltejs/kit').Config} */
+//** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
-		adapter: adapter()
-	}
-};
+	  adapter: adapter()
+	},
+	preprocess: [
+	  preprocess({
+		postcss: true,
+	  }),
+	],
+  };
+  
 
 export default config;
