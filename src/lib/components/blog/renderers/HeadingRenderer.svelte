67af5a0515facf4379357c624170b8b1db45305e
Ian Thompson <ianthompson@nicelion.com>
2022-12-25 00:50:36 -0500

    Initial commit

---
+++ src/lib/components/blog/renderers/HeadingRenderer.svelte
@@ -0,0 +1,23 @@
+<!--
+ /src/lib/components/blog/renderers/HeadingRenderer.svelte
+ HeadingRenderer.svelte
+ iancthompson
+ 
+ Created by Ian Thompson on December 21st 2022
+ ianthompson@nicelion.com
+ 
+ https://iancthompson.dev
+ 
+--->
+
+
+<script lang="ts">
+    export let depth;
+    export let text: string;
+
+    console.log("DATA", depth);
+</script>
+
+<h1 id={text} class="text-3xl my-6"><a href={`#${text}`} class="">📎</a> {text}</h1>
+
+
---
+++ src/lib/components/blog/renderers/ParagraphRenderer.svelte
@@ -0,0 +1,17 @@
+<!--
+ ParagraphRenderer.svelte
+ iancthompson
+ 
+ Created by Ian Thompson on December 21st 2022
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2022 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+--->
+
+<script>
+    export let text = undefined
+</script>
+  
+<p class="">{text}</p>
