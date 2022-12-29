b61267b60525bc8773070edf5ac2b65d5e90c184
Ian Thompson <ianthompson@nicelion.com>
2022-12-29 16:52:12 -0500

    Initial commit

---
+++ src/lib/components/AdContainer.svelte
@@ -0,0 +1,19 @@
+<!--
+ /src/lib/components/AdContainer.svelte
+ AdContainer.svelte
+ iancthompson
+ 
+ Created by Ian Thompson on December 29th 2022
+ ianthompson@nicelion.com
+ 
+ https://iancthompson.dev
+ 
+--->
+
+<script lang="ts">
+
+</script>
+
+<div class="w-full bg-red-400 text-center">
+    this is an ad!
+</div>
