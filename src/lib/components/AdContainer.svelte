3d696d5f3835660d9e5b558925332d638e14c161
Ian Thompson <ianthompson@nicelion.com>
2023-01-01 16:43:35 -0500

    Added google advertisement

--- src/lib/components/AdContainer.svelte
+++ src/lib/components/AdContainer.svelte
@@ -15,8 +15,18 @@
 </script>
 
 <div class="w-full my-3">
-    <div class="w-full bg-red-400 text-center py-6 text-black">
-        this is an ad!
+    <div class="w-full">
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4487298036056646"
+            crossorigin="anonymous"></script>
+        <ins class="adsbygoogle"
+            style="display:block; text-align:center;"
+            data-ad-layout="in-article"
+            data-ad-format="fluid"
+            data-ad-client="ca-pub-4487298036056646"
+            data-ad-slot="1076290283"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
     </div>
     <div class="text-right text-xs mt-2">
         <label for="why-ads-modal" class="hover:underline cursor-pointer">Why ads?</label>
