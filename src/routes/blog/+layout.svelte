380d9f1ea44277dd7ff88fc7f38f124c0e1e573e
Ian Thompson <ianthompson@nicelion.com>
2023-01-01 16:44:06 -0500

    Remove breadcrumbs

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -10,11 +10,14 @@
  
 --->
 
-<script>
+<script lang="ts">
 	import AdContainer from "$lib/components/AdContainer.svelte";
 	import Badge from "$lib/components/Badge.svelte";
 	import RefineSearch from "$lib/components/blog/layout/RefineSearch.svelte";
+  
+  import type { PageData } from './$types';
 
+  export let data: PageData;
 </script>
 
 <div class="flex flex-col md:flex-row w-full h-full p-4 ">
@@ -23,11 +26,12 @@
       <AdContainer />
     </div>
     <main class="w-full md:px-8">
-        <div class="text-sm breadcrumbs">
+        <!-- <div class="text-sm breadcrumbs">
             <ul>
               <li><a href="/blog" class="url">Blog</a></li> 
+              <li>Blog Post Title</li> 
             </ul>
-          </div>
+          </div> -->
         <slot />
         <AdContainer />
 
