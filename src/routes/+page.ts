38d3841e8b4e8901de984c6a3261d117d491a798
Ian Thompson <ianthompson@nicelion.com>
2023-10-15 18:24:21 -0400

    made to prerender

---
+++ src/routes/+page.ts
@@ -0,0 +1,13 @@
+/**
+ +page.ts
+ iancthompson
+ 
+ Created by Ian Thompson on October 15th 2023
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2023 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+*/
+
+export const prerender = true;
