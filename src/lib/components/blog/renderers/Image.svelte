f31c6ce947625888a82e3b2af862e4e97f092ee1
Ian Thompson <ianthompson@nicelion.com>
2023-10-16 15:29:37 -0400

    Added padding

--- src/lib/components/blog/renderers/Image.svelte
+++ src/lib/components/blog/renderers/Image.svelte
@@ -19,7 +19,7 @@
     
 </script>
 
-<div class="flex flex-col justify-center items-center space-y-3 my-6">
+<div class="flex flex-col justify-center items-center space-y-3 my-6 align-middle">
     <img src={src} alt={alt} class="h-96 rounded-md  object-contain">
-    <p class="italic">{alt}</p>
+    <p class="italic text-center px-12">{alt}</p>
 </div>
