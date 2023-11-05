6811d8eb338cbd1f36bc0e0122412eeed1b44e4e
Ian Thompson <ianthompson@nicelion.com>
2023-11-05 14:20:51 -0500

    add retroactive banner

---
+++ src/lib/components/blog/Retroactive.svelte
@@ -0,0 +1,15 @@
+<!--
+ Retroactive.svelte
+ iancthompson
+ 
+ Created by Ian Thompson on October 27th 2023
+ ianthompson@nicelion.com
+ https://www.nicelion.com
+ 
+ Copyright (c) 2023 Nice Lion Technologies LLC. All Rights Reserved.
+ 
+--->
+
+<div class="alert text-white">
+	<p>This was posted retroactively. <a href="/" class="url">What does this mean?</a></p>
+</div>
